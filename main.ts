dc28b7937fd23baacc4741e1f7f9559055df26bd
AlejandroPardoS <a.pardos2@uniandes.edu.co>
2024-04-17 21:44:24 -0500

    listos

--- main.ts
+++ main.ts
@@ -8,14 +8,16 @@ function showData(series: Serie[]): void {
     let seriesTableBody: HTMLElement = document.createElement("tbody");
     for(let serie of series) {
         let serieRow: HTMLElement = document.createElement("tr");
+        serieRow.addEventListener("click", listenerShowMoreData);
         serieRow.innerHTML = `<td class="table-active"><b>${serie.id}</b></td>
-                              <td class="table-active" style="color:cornflowerblue">${serie.name}</td>
+                              <td class="table-active serie-name" style="color:cornflowerblue">${serie.name}</td>
                               <td class="table-active">${serie.channel}</td>
                               <td class="table-active">${serie.seasons}</td>`;
         seriesTableBody.appendChild(serieRow);
     }
     seriesTabla.appendChild(seriesTableBody);
 }
+
 
 let average: HTMLElement = document.getElementById("average")! ; 
 showAverage(series)
@@ -27,4 +29,28 @@ function showAverage(series: Serie[]): void {
     }
     let averageSeasons: number = totalSeasons / series.length;
     average.innerHTML = `Season average: ${averageSeasons}`;
+}
+
+let moreData: HTMLElement = document.getElementById("info")! ; 
+
+function listenerShowMoreData(ev: Event) {
+    var target = ev.target as HTMLElement;
+    var serieIdElement = target.parentElement?.querySelector("td:first-child b");
+    var serieIdText: string = serieIdElement ? serieIdElement.textContent || "" : "";
+    var serieId: number = parseInt(serieIdText, 10);
+    console.log("ID de la serie:", serieId);
+    for(let serie of series) {
+        if (serie.id == serieId) {
+            var image = serie.imageUrl;
+            console.log("Imagen de la serie:", image);
+            moreData.innerHTML=`<div class="card">
+                                    <img src="${image}" class="card-img-top" alt="Series image" id="image" width="70%">
+                                    <div class="card-body">
+                                        <h5 class="card-title">${serie.name}</h5>
+                                        <p class="card-text">${serie.description}</p>
+                                        <a class="btn btn-primary" href="${serie.website}">Mas Información</a>
+                                    </div>
+                                </div>`;
+        }
+    }
 }
--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,8 @@ function showData(series) {
     for (var _i = 0, series_1 = series; _i < series_1.length; _i++) {
         var serie = series_1[_i];
         var serieRow = document.createElement("tr");
-        serieRow.innerHTML = "<td class=\"table-active\"><b>".concat(serie.id, "</b></td>\n                              <td class=\"table-active\" style=\"color:cornflowerblue\">").concat(serie.name, "</td>\n                              <td class=\"table-active\">").concat(serie.channel, "</td>\n                              <td class=\"table-active\">").concat(serie.seasons, "</td>");
+        serieRow.addEventListener("click", listenerShowMoreData);
+        serieRow.innerHTML = "<td class=\"table-active\"><b>".concat(serie.id, "</b></td>\n                              <td class=\"table-active serie-name\" style=\"color:cornflowerblue\">").concat(serie.name, "</td>\n                              <td class=\"table-active\">").concat(serie.channel, "</td>\n                              <td class=\"table-active\">").concat(serie.seasons, "</td>");
         seriesTableBody.appendChild(serieRow);
     }
     seriesTabla.appendChild(seriesTableBody);
@@ -21,4 +22,21 @@ function showAverage(series) {
     }
     var averageSeasons = totalSeasons / series.length;
     average.innerHTML = "Season average: ".concat(averageSeasons);
+}
+var moreData = document.getElementById("info");
+function listenerShowMoreData(ev) {
+    var _a;
+    var target = ev.target;
+    var serieIdElement = (_a = target.parentElement) === null || _a === void 0 ? void 0 : _a.querySelector("td:first-child b");
+    var serieIdText = serieIdElement ? serieIdElement.textContent || "" : "";
+    var serieId = parseInt(serieIdText, 10);
+    console.log("ID de la serie:", serieId);
+    for (var _i = 0, series_3 = series; _i < series_3.length; _i++) {
+        var serie = series_3[_i];
+        if (serie.id == serieId) {
+            var image = serie.imageUrl;
+            console.log("Imagen de la serie:", image);
+            moreData.innerHTML = "<div class=\"card\">\n                                    <img src=\"".concat(image, "\" class=\"card-img-top\" alt=\"Series image\" id=\"image\" width=\"70%\">\n                                    <div class=\"card-body\">\n                                        <h5 class=\"card-title\">").concat(serie.name, "</h5>\n                                        <p class=\"card-text\">").concat(serie.description, "</p>\n                                        <a class=\"btn btn-primary\" href=\"").concat(serie.website, "\">Mas Informaci\u00F3n</a>\n                                    </div>\n                                </div>");
+        }
+    }
 }
