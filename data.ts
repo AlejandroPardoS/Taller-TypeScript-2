a02c00fb3bad4034a3ede3de58ffb9f96f55ae8f
AlejandroPardoS <a.pardos2@uniandes.edu.co>
2024-04-17 21:50:23 -0500

    TS 2

--- data.ts
+++ data.ts
@@ -2,7 +2,7 @@ import { Serie } from "./Serie.js";
 
 export const series = [
     new Serie (1,"Breaking Bad","AMC", 5,"Set and filmed in Albuquerque, New Mexico, the series tells the story of Walter White, a struggling and depressed high school chemistry teacher who is diagnosed with lung cancer" ,
-    "https://www.amc.com/shows/breaking-bad","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREjh5ADrp5Soc37MLqJfTuNHdU5_acbHUMUdbS_70dLw&s") ,
+    "https://www.netflix.com/co/title/70143836","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREjh5ADrp5Soc37MLqJfTuNHdU5_acbHUMUdbS_70dLw&s") ,
     
     new Serie (2,"Orange Is the New Black", "Netflix", 6, "The series begins revolving around Piper Chapman, a woman in her thirties living in New York City who is sentenced to 15 months in Litchfield Penitentiary", 
     "https://www.netflix.com/co/title/70242311","https://svijetfilma.eu/wp-content/uploads/2019/06/OITNB_Main-768x486.jpg"),
@@ -11,7 +11,7 @@ export const series = [
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6NRw6WM06DUZiE0jDIhwudQim9xTrXSMyd2XveGONTA&s"),
 
     new Serie (4, "The Big Bang Theory", "CBS", 12, "Leonard and Sheldon are brilliant physicists—geniuses in the laboratory but socially challenged everywhere else. Enter beautiful, street-smart neighbor Penny, who aims to teach them a thing or two about life. Despite their on-again, off-again relationship in the past, Leonard and Penny have finally gotten married. Even Sheldon has found a female companion, entering into a relationship agreement with neurobiologist Amy Farrah Fowler, and he recently took their relationship to the next level by marrying her after a long courtship. In their free time, Leonard and Sheldon enjoy fantasy role-playing games with their ever-expanding universe of friends, including fellow scientists Koothrappali, Wolowitz, and Wolowitz’s adorable microbiologist wife, Bernadette, who is adjusting to life with their two children.",
-        "https://www.cbs.com/shows/big_bang_theory/about/", "https://beam-images.warnermediacdn.com/BEAM_LWM_DELIVERABLES/c8ea8e19-cae7-4683-9b62-cdbbed744784/efa21afd-d650-11ee-a7fe-0288acd57a89?host=wbd-images.prod-vod.h264.io&partner=beamcom"),
+        "https://www.imdb.com/title/tt0898266/", "https://beam-images.warnermediacdn.com/BEAM_LWM_DELIVERABLES/c8ea8e19-cae7-4683-9b62-cdbbed744784/efa21afd-d650-11ee-a7fe-0288acd57a89?host=wbd-images.prod-vod.h264.io&partner=beamcom"),
     
     new Serie (5, "Sherlock", "BBC",  4, "Sherlock depicts consulting detective Sherlock Holmes (Benedict Cumberbatch) solving various mysteries in modern-day London. Holmes is assisted by his flatmate and friend, Dr John Watson (Martin Freeman), who has returned from military service in Afghanistan with the Royal Army Medical Corps",
         "https://www.bbc.co.uk/programmes/b018ttws", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGMC75yAQ5tdIFJ_CVwtw1tySsPprl_d5Fu8q8T0NHWw&s"),
